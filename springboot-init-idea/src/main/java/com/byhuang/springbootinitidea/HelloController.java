d78de6e4ff0547ab8306d9d07223d1823909caa8
mskj-huangbingyi <byhuang1998@gmail.com>
2022-11-02 22:41:04 +0800

    获取yaml数据

--- springboot-init-idea/src/main/java/com/byhuang/springbootinitidea/HelloController.java
+++ springboot-init-idea/src/main/java/com/byhuang/springbootinitidea/HelloController.java
@@ -1,5 +1,9 @@
 package com.byhuang.springbootinitidea;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +16,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${name}")
+    private String name;
+
+    @Value("${person.age}")
+    private int age;
+
+    @Value("${address[0]")
+    private String address1;
+
+    @Value("${str1}")
+    private String str1;
+
+    @Value("${str2}")
+    private String str2;
+
+    @Autowired
+    private Environment env;
+
     @RequestMapping("/hello")
     public String hello() {
+        System.out.println("========直接通过注解来来一个个获取yaml文件中的数据========");
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address1);
+        System.out.println(str1);
+        System.out.println(str2);
+
+        System.out.println("========可以通过Environment变量来直接获取yaml文件中的数据========");
+        System.out.println(env.getProperty("person.name"));
+
         return "hello springboot init...";
     }
 }
