3ddbe38eddab2b324de0327b776a12486a058ab0
mskj-huangbingyi <huangbingyi@mskj.com>
2022-11-02 21:16:55 +0800

    springboot repo init

---
+++ springboot-helloworld/src/main/java/com/byhuang/HelloApplication.java
@@ -0,0 +1,18 @@
+package com.byhuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mskj-huangbingyi
+ * @version 1.0
+ * @date 2022/11/2 21:08
+ * @description TODO
+ */
+@SpringBootApplication
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class);
+    }
+}
