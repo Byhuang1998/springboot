3b80a7928af891f37c06133555b60b691acf5172
mskj-huangbingyi <byhuang1998@gmail.com>
2022-11-03 10:23:33 +0800

    profile文件配置

---
+++ springboot-profile/src/test/java/com/byhuang/springbootprofile/SpringbootProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byhuang.springbootprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
